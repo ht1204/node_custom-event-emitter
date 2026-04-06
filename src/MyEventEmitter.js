9877e9f5539c323e2ba9ce43b33e738951f3e832
Hector Triana <ptb1204@hotmail.com>
2026-04-05 21:21:15 -0500

    feat: proposal approach event emitter

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,106 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this._events = new Map();
+  }
+
+  on(eventName, listener) {
+    if (!this._events.has(eventName)) {
+      this._events.set(eventName, []);
+    }
+    this._events.get(eventName).push({ listener, once: false });
+
+    return this;
+  }
+
+  once(eventName, listener) {
+    if (!this._events.has(eventName)) {
+      this._events.set(eventName, []);
+    }
+    this._events.get(eventName).push({ listener, once: true });
+
+    return this;
+  }
+
+  off(eventName, listener) {
+    if (!this._events.has(eventName)) {
+      return this;
+    }
+
+    const listeners = this._events.get(eventName);
+    const index = listeners.findIndex((item) => item.listener === listener);
+
+    if (index !== -1) {
+      listeners.splice(index, 1);
+    }
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    if (!this._events.has(eventName)) {
+      return false;
+    }
+
+    const listeners = this._events.get(eventName);
+
+    if (listeners.length === 0) {
+      return false;
+    }
+
+    const toRemove = [];
+
+    listeners.forEach((item, index) => {
+      item.listener(...args);
+
+      if (item.once) {
+        toRemove.push(index);
+      }
+    });
+
+    for (let i = toRemove.length - 1; i >= 0; i--) {
+      listeners.splice(toRemove[i], 1);
+    }
+
+    return true;
+  }
+
+  prependListener(eventName, listener) {
+    if (!this._events.has(eventName)) {
+      this._events.set(eventName, []);
+    }
+    this._events.get(eventName).unshift({ listener, once: false });
+
+    return this;
+  }
+
+  prependOnceListener(eventName, listener) {
+    if (!this._events.has(eventName)) {
+      this._events.set(eventName, []);
+    }
+    this._events.get(eventName).unshift({ listener, once: true });
+
+    return this;
+  }
+
+  removeAllListeners(eventName) {
+    if (eventName === undefined) {
+      this._events.clear();
+    } else {
+      this._events.delete(eventName);
+    }
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    if (!this._events.has(eventName)) {
+      return 0;
+    }
+
+    return this._events.get(eventName).length;
+  }
 }
 
 module.exports = MyEventEmitter;
